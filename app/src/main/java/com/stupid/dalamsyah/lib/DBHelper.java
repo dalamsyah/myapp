5ac2619c7c2679298dfd6caee3470582082c002f
sapuser <sapuser>
2019-05-15 21:00:27 +0700

    first commit

---
+++ app/src/main/java/com/stupid/dalamsyah/lib/DBHelper.java
@@ -0,0 +1,26 @@
+package com.stupid.dalamsyah.lib;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    // If you change the database schema, you must increment the database version.
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "myapp.db";
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
